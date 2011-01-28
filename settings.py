efc31f521fa39dab5114c560300b6e8eb187755a
Garrett Robinson <garrett@mint.(none)>
2011-01-28 16:56:37 -0500

    Changed path to database to reflect new home on this Linux box. RIP Macbook Pro. Tested, site is working as it was.

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/grobin/Documents/Django-Projects/apprising/appalachia.db', # Or path to database file if using sqlite3.
+        'NAME': '/home/garrett/Documents/webdev/apprising/appalachia.db', # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
