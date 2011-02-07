0a4b296e2803bfb863717a3ca924c68ca719ff93
handsomeransoms <handsomeransoms@gmail.com>
2011-02-06 21:53:27 -0500

    And... Zinnia works! Beautiful. Now time to design templates and makes this thing look like a real website.
    Only one problem: I'm not sure if the MEDIA_URL settings are wired up
    100% correctly. Only say this because a thumbnail image for my example
    blog post was uploaded, but I didn't see it on the page. However, it may
    just not have been rendered by the super-barebones default template.
    Investigate later.

--- settings.py
+++ settings.py
@@ -112,6 +112,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'django.contrib.comments',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
